c2263571e3727fed7b13a84a342b3fecb3743d04
ssdd08 <H098S02204@chunchon.gwe.hs.kr>
2025-11-25 09:00:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,6 +74,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
+    labels[0] : 
 }
 
 # ======================
